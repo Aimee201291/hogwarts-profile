6ade234afcb0153d20783c750f3be68d1ea1cd18
Aimee Rodríguez <arodriguez.posma@gmail.com>
2021-04-05 17:44:15 -0430

    Mostrando tabla de personajes

--- src/app/character/components/characters/characters.component.ts
+++ src/app/character/components/characters/characters.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Character } from './../../../core/models/character.model';
+import { CharactersService } from './../../../core/services/characters/characters.service';
+
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersComponent implements OnInit {
 
-  constructor() { }
+  characters: Character[] = [];
+
+  constructor(
+    private charactersService: CharactersService
+  ) { }
 
   ngOnInit(): void {
+    this.fetchCharacters();
+  }
+
+  fetchCharacters() {
+    this.charactersService.getAllCharacters()
+    .subscribe(characters => {
+      this.characters = characters;
+    });
   }
 
 }
---
+++ src/app/core/models/character.model.ts
@@ -0,0 +1,7 @@
+export interface Character {
+    id: string;
+    name: string;
+    patronus: number;
+    age: Date;
+    image: string;
+}  
